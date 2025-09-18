f50e95a1b2f46ecd27a72497d07f04b311bc7ff9
ziyane <ziyane.tatiana@starthmore.edu>
2025-09-19 01:36:36 +0300

    Convert PHPMailer from submodule to regular files

---
+++ Plugins/PHPMailer/test/Fixtures/LocalizationTest/phpmailer.lang-yy.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Test fixture.
+ *
+ * Used in the `PHPMailer\LocalizationTest` to test that arbitrary code in translation files is disregarded.
+ */
+
+$composer = file_get_contents(__DIR__ . '/../../../composer.json');
+
+echo $composer;
+
+$PHPMAILER_LANG['extension_missing'] = 'Confirming that test fixture was loaded correctly (yy).';
+$PHPMAILER_LANG['empty_message']     = $composer;
+$PHPMAILER_LANG['execute']           = exec('some harmful command');
+$PHPMAILER_LANG['signing']           = "Double quoted but not interpolated $composer";
